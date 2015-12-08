00cdf90f9640da8191c08776b3928a03d551a131
cwilson04 <cassiewilson4@gmail.com>
2015-12-08 20:33:22 +0000

    corrected checkpoint-28-models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
-  let(:comment) { comment.create!(body: "Comment Body", post: post) }
+  let(:comment) { Comment.create!(body: "Comment Body", post: post) }
   
   describe "attributes" do
     it "should respond to body" do
