a94cbed915ea42dc036d1b0420f8f72e3259b379
cwilson04 <cassiewilson4@gmail.com>
2015-12-11 18:31:56 +0000

    Halfway through checkpoint 32

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -22,13 +22,40 @@ RSpec.describe PostsController, type: :controller do
   #     get :show
   #     expect(response).to have_http_status(:success)
   #   end
-  # end
+    
 
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+    
+    it "instantiates @post" do
+      get :new
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+  
+  describe "post create" do
+    it "increases the number of posts by 1" do
+      expect {post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph} }
+    end
+    
+    it "assigns the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(assigns(:post)).to eq Post.last
+    end
+    
+    it "redirects to a new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to Post.last
+    end
+  end
   # end
 
   # describe "GET #edit" do
