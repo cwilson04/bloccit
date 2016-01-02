a33e92dbeff40a2165ddb35f8eab39b79d082d36
cwilson04 <cassiewilson4@gmail.com>
2016-01-02 13:06:21 +0000

    corrected banners at sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
       create_session(@user)
       redirect_to root_path
     else
-      flash_now[:alert] = "There was an error creating your account. Please try again."
+      flash.now[:alert] = "There was an error creating your account. Please try again."
       render :new
     end
   end
