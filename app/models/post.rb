50d026d40815f1c023616847b33fa73cda257a48
cwilson04 <cassiewilson4@gmail.com>
2016-01-13 20:43:30 +0000

    work in progress on checkpoint 43

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   
@@ -12,4 +13,15 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
   
+  def up_votes
+    votes.where(value: 1).count
+  end
+  
+  def down_votes
+    votes.where(value: -1).count
+  end
+  
+  def points
+    votes.sum(:value)
+  end
 end
