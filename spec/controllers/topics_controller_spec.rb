896df891d79395c4b31b0006a914ff47396ac2ff
cwilson04 <cassiewilson4@gmail.com>
2016-01-27 19:48:05 +0000

    resolved test in topics controller spec

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -122,35 +122,18 @@ RSpec.describe TopicsController, type: :controller do
     
     
     describe "GET new" do
-      it "returns http success" do
+      it "returns http redirect" do
         get :new
-        expect(response).to have_http_status(:success)
+        expect(response).to redirect_to(topics_path)
       end
-      
-      it "renders the #new view" do
-        get :new
-        expect(response).to render_template(:new)
-      end
-      
-      it "initializes @topic" do
-        get :new
-        expect(assigns(:topic)).not_to be_nil
-      end
+
     end
     
     describe "POST create" do
-      it "increases the number of topics by 1" do
-        expect{ post :create, {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}}.to change(Topic,:count).by(1)
-      end
       
-      it "assigns Topic.last to @topic" do
-        post :create, {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}
-        expect(assigns(:topic)).to eq Topic.last
-      end
-      
-      it "redirects to the new topic" do
+      it "redirects to topic show page" do
          post :create, {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}
-         expect(response).to redirect_to Topic.last
+         expect(response).to redirect_to(topics_path)
        end
      end
      
