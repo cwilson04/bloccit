1ab4bf4d8fedf4a324c85aa97999f4d88dc819e8
cwilson04 <cassiewilson4@gmail.com>
2016-02-08 21:36:57 +0000

    errors in rspec for no such column update_at

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Api::V1::CommentsController, type: :controller do
   let(:my_topic) { create(:topic) }
   let(:my_user) { create(:user) }
   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
-  let(:my_comment) { Comment.create!(body: 'Comment body', post: my_post, user: my_user) }
+  let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) }
   
   context "unauthenticated user" do
     it "GET index returns http success" do
@@ -16,6 +17,13 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
       get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
     end
+    
+    it "GET show returns a comment" do
+      get :show, id: my_comment.id
+      response_hash = JSON.parse response.body
+      expect(response_hash['id']).to eq my_comment.id
+      expect(response_has['body']).to eq my_comment.body
+    end
   end
   
   context "unauthorized user" do
@@ -23,7 +31,7 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
     end
     
-    it "GET index returns http success" do
+     it "GET index returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
@@ -31,6 +39,13 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
     it "GET show returns http success" do
       get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
+    end
+    
+    it "GET show returns a comment" do
+      get :show, id: my_comment.id
+      response_hash = JSON.parse response.body
+      expect(response_hash['id']).to eq my_comment.id
+      expect(response_has['body']).to eq my_comment.body
     end
   end
 end
